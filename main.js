80c7122ccda946ed795717f6647ab257a878314c
jaswant-rajput <jaswant.s@somaiya.edu>
2023-08-20 18:02:16 +0530

    add the the first fetch request

--- main.js
+++ main.js
@@ -2,16 +2,18 @@
 
 
 
-import { OpenAI } from "openai"
+// import { OpenAI } from "openai"
 
 
-const openai = new OpenAI({
-  apiKey : process.env.apiKey,
-  dangerouslyAllowBrowser: true
-})
+// const openai = new OpenAI({
+//   apiKey : process.env.apiKey,
+//   dangerouslyAllowBrowser: true
+// })
 
 
 const button = document.getElementById("send-btn")
+const url = "https://warm-snickerdoodle-ee4999.netlify.app/.netlify/functions/fetchAI"
+
 
 button.addEventListener("click",function(){
   const userInput = document.getElementById("setup-textarea").value
@@ -24,27 +26,40 @@ button.addEventListener("click",function(){
 async function createBotReply(outline){  
   const bossMessage = document.getElementById("boss-message")
   bossMessage.innerText = "Ok, just wait a second while my digital brain digests that..."
-  const completion = await openai.completions.create({
-    model : "text-davinci-002",
-    prompt : `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
-    ###
-    outline: Two dogs fall in love and move to Hawaii to learn to surf.
-    message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
-    ###
-    outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
-    message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
-    ###
-    outline: A group of corrupt lawyers try to send an innocent woman to jail.
-    message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
-    ###
-    outline: ${outline}
-    message: 
-    `,
-    max_tokens : 128,
+  // const completion = await openai.completions.create({
+  //   model : "text-davinci-002",
+  //   prompt : `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+  //   ###
+  //   outline: Two dogs fall in love and move to Hawaii to learn to surf.
+  //   message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+  //   ###
+  //   outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+  //   message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+  //   ###
+  //   outline: A group of corrupt lawyers try to send an innocent woman to jail.
+  //   message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+  //   ###
+  //   outline: ${outline}
+  //   message: 
+  //   `,
+  //   max_tokens : 128,
+  // })
+  // const reply = completion.choices[0].text
+  // bossMessage.innerText = `${reply}`
+  // createSynopsis(outline)
+
+  const response = await fetch(url,{
+    method:"POST",
+    headers: {
+      "content-type":"text/plain"
+    },
+    body : outline
   })
-  const reply = completion.choices[0].text
-  bossMessage.innerText = `${reply}`
-  createSynopsis(outline)
+
+  const data = await response.json()
+  console.log(data)
+
+  
 }
 
 async function createSynopsis(outline){
