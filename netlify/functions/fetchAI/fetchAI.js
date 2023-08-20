f3a03a4efb2e9963d5619bfc9522a9d99c8bea0f
jaswant-rajput <jaswant.s@somaiya.edu>
2023-08-20 18:28:07 +0530

    previous part .2

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -2,7 +2,7 @@ const { default: OpenAI } = require("openai")
 
 
 const openai = new OpenAI({
-  apiKey : process.env.OPENAI_API_KEY
+  apiKey : process.env.OPENAI_API_KEY,
 })
 
 
@@ -10,7 +10,7 @@ const handler = async (event) => {
   try {
 
 
-    const subject = event.queryStringParameters.name || 'World'
+    
     return {
       statusCode: 200,
       body: JSON.stringify({ 
